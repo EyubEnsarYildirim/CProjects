42e7e309c5f2854d7bd96cb128967a8b90244b11
EyubEnsarYildirim <ensary6107@gmail.com>
2021-10-09 22:37:53 +0300

    Yeni Dosyalar Eklendi

---
+++ artma azalma.cpp
@@ -0,0 +1,103 @@
+#include<stdio.h>
+
+int main   ()  {
+	
+	
+	int y;
+	int x;
+	int a;
+	
+	
+	
+	
+	y=4;
+	a=2;
+	x = y+ a+4;
+	printf("x:%d y :%d a:%d\n",x ,y,a);
+	 
+	
+	
+	y=5;
+	x=4;
+	a=2;
+	
+	
+x=a+4;
+printf("y:%d a:%d x:%d\n",++++y,++++a,----x);
+
+	
+	
+	
+	
+int t;
+int h;
+int g;
+
+
+t=8;
+h=7;
+g=6;
+
+t=g-2;
+printf("t:%d h:%d g:%d\n",++t,++h,++g);
+	
+	
+	
+	int j;
+	int b;
+	int c;
+	
+	j=8;
+	b=4;
+	c=2;
+	
+	j=b+6;
+	printf("j:%d b:%d c:%d\n",j,b,c);
+	
+	
+	int s;
+	int e;
+	int i;
+	
+	s=2;
+	e=3;
+	i=5;
+s=i+4;	
+	printf("s:%d e:%d i:%d",++s,++e,--i);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return  0;
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
---
+++ ensar.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main () {
+	
+	
+	int x;
+	int y;
+
+	
+	x=8;
+	y=5;
+	
+	
+	printf ("merhaba yazilimci\n ");
+	printf ("nasilsin\n ");
+	printf("x = %d  \n  y = %d \n",x,y);
+	printf("x+y = %d", x+y)  ;
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+
