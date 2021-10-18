826eb524d26a09f8e9307caf38f0cb1b780249e7
EyubEnsarYildirim <ensary6107@gmail.com>
2021-10-18 23:04:32 +0300

    girişçıkışfonksiyonları

---
+++ girişçıkışfonksiyonları.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#define PI 3.14
+
+int main () {
+	
+	
+	
+	int yaricap;
+	float hacim;
+	
+	
+	printf("kurenin yarýçapýný giriniz:");
+	scanf("%d",&yaricap);
+	
+	hacim =(4/3.0)*PI*(yaricap*yaricap*yaricap);
+	printf("kurenin hacmi %.2f 'dir",hacim);
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
