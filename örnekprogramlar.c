13e62fa8f1af4ca0f5aaa38ca6a377bb27e34070
EyubEnsarYildirim <ensary6107@gmail.com>
2021-10-20 19:24:16 +0300

    örnekprogramlar

---
+++ örnekprogramlar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main () {
+	
+	
+	int a,b,c,d,e;
+	
+	float aritmatik;
+	
+	printf("5 tane sayi giriniz:");
+	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	aritmatik =(a+b+c+d+e)/5.0;
+	printf("girdiginiz sayilarin aritmatik ortalamasý %.2f",aritmatik);
+	
+	
+	
+	
+	return 0;
+}
