f004c837f511b539813efa476121c536980f326b
EyubEnsarYildirim <ensary6107@gmail.com>
2021-10-21 21:15:38 +0300

    koşulluifadeler

---
+++ koşulluifadeler.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+	
+	
+	int note;
+	printf("notunuzu girin:");
+	scanf("%d",&note);
+	
+	if (note > 60){
+		
+		
+	printf("dersten gectiniz:");
+		
+		
+		
+		
+		
+	}
+	else {
+		
+		
+	printf ("dersten kaldiniz:");
+		
+		
+	}
+	
+	return 0 ;
+	
+	
+	
+	
+}
